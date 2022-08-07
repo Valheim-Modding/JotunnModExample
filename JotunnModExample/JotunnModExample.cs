9d53927711847ce618fbd87f61c8009ce955aed7
Jules <github@sirskunkalot.de>
2022-08-07 23:26:43 +0200

    use jötunn v2.7.7

--- JotunnModExample/JotunnModExample.cs
+++ JotunnModExample/JotunnModExample.cs
@@ -34,7 +34,7 @@ namespace JotunnModExample
         // BepInEx' plugin metadata
         public const string PluginGUID = "com.jotunn.JotunnModExample";
         public const string PluginName = "JotunnModExample";
-        public const string PluginVersion = "2.7.6";
+        public const string PluginVersion = "2.7.7";
 
         // Your mod's custom localization
         private CustomLocalization Localization;
